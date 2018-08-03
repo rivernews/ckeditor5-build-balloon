3ba4cd3d62139eaf0aab23cbb9119b0ce7ef2940
Shaung Cheng <shaungc@umich.edu>
2018-08-03 03:12:48 -0400

    try to add 3rd party media plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -24,7 +24,7 @@ import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import * as MediaPlugin from "@akilli/ckeditor5-media";
+import * as MediaPlugin from "@akilli/ckeditor5-media/src/media";
 
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';

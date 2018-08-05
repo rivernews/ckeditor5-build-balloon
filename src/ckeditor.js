27f6f8ef0186338e55d648e5d1019d711c39c14f
Shaung Cheng <shaungc@umich.edu>
2018-08-05 10:10:25 -0400

    restore to default but + code & strikethrough

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -9,30 +9,18 @@ import BalloonEditorBase from '@ckeditor/ckeditor5-editor-balloon/src/balloonedi
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
-
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
-
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import InsertUrlImage from "./insert-image-by-url/insert-image-by-url";
-
-// import Embed from 'ckeditor5-embed/src/embed';
-
-// import Media from './ckeditor5-media/src/media';
-// import MediaCaption from './ckeditor5-media/src/mediacaption';
-// import MediaStyle from './ckeditor5-media/src/mediastyle';
-// import MediaToolbar from './ckeditor5-media/src/mediatoolbar';
-// import MediaBrowser from './ckeditor5-media/src/mediabrowser';
-
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
@@ -44,23 +32,18 @@ BalloonEditor.builtinPlugins = [
 	Essentials,
 	UploadAdapter,
 	Autoformat,
-
 	Bold,
 	Italic,
 	Strikethrough,
 	Code,
-
 	BlockQuote,
+	EasyImage,
 	Heading,
-
 	Image,
 	ImageCaption,
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
-
-	InsertUrlImage,
-
 	Link,
 	List,
 	Paragraph
@@ -72,7 +55,7 @@ BalloonEditor.defaultConfig = {
 		items: [
 			'heading', 'code', 'blockQuote', '|',
 			'bold', 'italic', 'strikethrough', 'bulletedList', 'numberedList', '|',
-			'imageUpload', 'insertUrlImage', '|', 'link'
+			'imageUpload', '|', 'link'
 		],
 	},
 
